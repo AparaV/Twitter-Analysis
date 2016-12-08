12c8faaa97795f13d8d6d1246588e04477f6df94
Apara <ssapar@gmail.com>
2016-12-08 15:07:53 -0700

    Re-format for PyCharm

--- mineTweets.py
+++ mineTweets.py
@@ -1,63 +1,62 @@
-import json
 import tweepy
 import time
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-#File containing API and Access credentials
+# File containing API and Access credentials
 config_fname = 'config'
 
-#App credentials
+# App credentials
 with open(config_fname, 'r') as f:
-	consumer_key = f.readline().replace('\n', '')
-	consumer_secret = f.readline().replace('\n', '')
-	access_token = f.readline().replace('\n', '')
-	access_secret = f.readline().replace('\n', '')
+    consumer_key = f.readline().replace('\n', '')
+    consumer_secret = f.readline().replace('\n', '')
+    access_token = f.readline().replace('\n', '')
+    access_secret = f.readline().replace('\n', '')
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-runTime = 5 * 60 #5 minutes
+runTime = 5 * 60  # 5 minutes
 
-#Listening to Live Tweets
+
+# Listening to Live Tweets
 class MyListener(StreamListener):
-	
-	def __init__(self):
-		self.startTime = time.time()
+    def __init__(self):
+        self.startTime = time.time()
 
-	def on_data(self, data):
+    def on_data(self, data):
 
-		if (time.time() - self.startTime) >= runTime:
-			return False
-			
-		try:
-			with open('liveStream.json', 'a') as f:
-				f.write(data)
-				return True
-		except BaseException as e:
-			print "Error"
-		return True
+        if (time.time() - self.startTime) >= runTime:
+            return False
 
-	def on_error(self, status_code):
-		print status_code
-		if status_code == 420:
-			print "Connection not established"
-		return False
+        try:
+            with open('liveStream.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print "Error"
+        return True
+
+    def on_error(self, status_code):
+        print status_code
+        if status_code == 420:
+            print "Connection not established"
+        return False
+
 
 if __name__ == "__main__":
-	
-	print "Enter a key word to track for 5 minutes. Be as specific as possible"
-	trackWord = str(raw_input())
-	
-	#Tracking live tweets with keyword
-	twitter_stream = Stream(auth, MyListener())
-	twitter_stream.filter(track = [trackWord])
+    print "Enter a key word to track for 5 minutes. Be as specific as possible"
+    trackWord = str(raw_input())
 
-	#Gathering tweets of a user
-	'''
+    # Tracking live tweets with keyword
+    twitter_stream = Stream(auth, MyListener())
+    twitter_stream.filter(track=[trackWord])
+
+    # Gathering tweets of a user
+    '''
 	print "Enter the user <screen_name> to track"
 	screenName = str(raw_input())
 	fname = screenName + "_tweets.json"
--- processTweets.py
+++ processTweets.py
@@ -9,7 +9,7 @@ from collections import Counter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-#Setup regex to ingore emoticons
+# Setup regex to ingore emoticons
 emoticons_str = r"""
 	(?:
 		[:=;] Eyes
@@ -17,98 +17,106 @@ emoticons_str = r"""
 		[D\)\]\(\]/\\OpP] # Mouth
 	)"""
 
-#Setup regex to split mentions, hashtags, urls, etc. together
+# Setup regex to split mentions, hashtags, urls, etc. together
 regex_str = [
-	emoticons_str,
-	r'<[^>]+>', #HTML tags
-	r'(?:@[\w_]+)', #@-mentions
-	r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", #hashtags
-	r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
-	r'(?:(?:\d+,?)+(?:\.?\d+)?)', #numbers
-	r"(?:[a-z][a-z'\-_]+[a-z])", #words with - and '
-	r'(?:[\w_]+)', #other words
-	r'(?:\S)' #anything else
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hashtags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
 ]
 
-tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
 
 def tokenize(s):
-	return tokens_re.findall(s)
+    return tokens_re.findall(s)
+
 
 def preprocess(s, lowercase=False):
-	tokens = tokenize(s)
-	if lowercase:
-		tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
-	for token in tokens:
-		token = token.encode('utf-8')
-	return tokens
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    for token in tokens:
+        token = token.encode('utf-8')
+    return tokens
+
 
 punctuation = list(string.punctuation)
 others = ['RT', 'via', u'\u2026', 'The', u'\u2019', 'amp', 'nil']
 stop = stopwords.words('english') + punctuation + others
 
-#Find most common words
+
+# Find most common words
 def terms_only(fname, number):
-	with open(fname, 'r') as f:
-		count_all = Counter()
-		for line in f:
-			tweet = json.loads(line)
-			terms_stop = [term for term in preprocess(tweet.get('text', 'nil'))
-						  if term not in stop and not term.startswith(('#', '@'))]
-			count_all.update(terms_stop)
-		print(count_all.most_common(number))
-		return count_all.most_common(number)
+    with open(fname, 'r') as f:
+        count_all = Counter()
+        for line in f:
+            tweet = json.loads(line)
+            terms_stop = [term for term in preprocess(tweet.get('text', 'nil'))
+                          if term not in stop and not term.startswith(('#', '@'))]
+            count_all.update(terms_stop)
+        print(count_all.most_common(number))
+        return count_all.most_common(number)
 
-#Find most common hashtags
+
+# Find most common hashtags
 def hash_only(fname, number):
-	with open(fname, 'r') as f:
-		count_all = Counter()
-		for line in f:
-			tweet = json.loads(line)
-			terms_hash = [term for term in preprocess(tweet.get('text', 'nil'))
-						  if term not in stop if term.startswith('#')]
-			count_all.update(terms_hash)
-		print(count_all.most_common(number))
+    with open(fname, 'r') as f:
+        count_all = Counter()
+        for line in f:
+            tweet = json.loads(line)
+            terms_hash = [term for term in preprocess(tweet.get('text', 'nil'))
+                          if term not in stop if term.startswith('#')]
+            count_all.update(terms_hash)
+        print(count_all.most_common(number))
 
-#Find most common mentions
+
+# Find most common mentions
 def mentions_only(fname, number):
-	with open(fname, 'r') as f:
-		count_all = Counter()
-		for line in f:
-			tweet = json.loads(line)
-			terms_mentions = [term for term in preprocess(tweet.get('text', 'nil'))
-							  if term not in stop if term.startswith('@')]
-			count_all.update(terms_mentions)
-		print(count_all.most_common(number))
+    with open(fname, 'r') as f:
+        count_all = Counter()
+        for line in f:
+            tweet = json.loads(line)
+            terms_mentions = [term for term in preprocess(tweet.get('text', 'nil'))
+                              if term not in stop if term.startswith('@')]
+            count_all.update(terms_mentions)
+        print(count_all.most_common(number))
 
-#Find most common two-term occurances
+
+# Find most common two-term occurances
 def cooccurances(fname, number):
-	with open(fname, 'r') as f:
-		com = collections.defaultdict(lambda: collections.defaultdict(int))
+    with open(fname, 'r') as f:
+        com = collections.defaultdict(lambda: collections.defaultdict(int))
 
-		for line in f:
-			tweet = json.loads(line)
-			terms_only = [term for term in preprocess(tweet.get('text', 'nil'))
-						  if term not in stop and not term.startswith(('#', '@'))]
-			for i in range(len(terms_only)):
-				for j in range(i+1, len(terms_only)):
-					w1, w2 = sorted([terms_only[i], terms_only[j]])
-					if w1 != w2:
-						com[w1][w2] += 1
-		com_max = []
-		for t1 in com:
-			t1_max_terms = sorted(com[t1].items(), key=operator.itemgetter(1), reverse=True)[:number]
-			for t2, t2_count in t1_max_terms:
-				com_max.append(((t1, t2), t2_count))
-		terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
-		print(terms_max[:number])
+        for line in f:
+            tweet = json.loads(line)
+            terms_only = [term for term in preprocess(tweet.get('text', 'nil'))
+                          if term not in stop and not term.startswith(('#', '@'))]
+            for i in range(len(terms_only)):
+                for j in range(i + 1, len(terms_only)):
+                    w1, w2 = sorted([terms_only[i], terms_only[j]])
+                    if w1 != w2:
+                        com[w1][w2] += 1
+        com_max = []
+        for t1 in com:
+            t1_max_terms = sorted(com[t1].items(), key=operator.itemgetter(1), reverse=True)[:number]
+            for t2, t2_count in t1_max_terms:
+                com_max.append(((t1, t2), t2_count))
+        terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
+        print(terms_max[:number])
 
-#Main Function Begins
+
+# Main Function Begins
 if __name__ == "__main__":
-	fname = "liveStream.json"
-	number = 10
-	'''
+    fname = "liveStream.json"
+    number = 10
+    '''
 	print "Terms only"
 	terms_only(fname, number)
 
@@ -122,28 +130,28 @@ if __name__ == "__main__":
 	cooccurances(fname, number)
 	'''
 
-	word_freq = terms_only(fname, 20)
-	labels, freq = zip(*word_freq)
-	data = {'data': freq, 'x': labels}
-	bar = vincent.Bar(data, iter_idx = 'x')
-	bar.to_json('term_freq.json')
+    word_freq = terms_only(fname, 20)
+    labels, freq = zip(*word_freq)
+    data = {'data': freq, 'x': labels}
+    bar = vincent.Bar(data, iter_idx='x')
+    bar.to_json('term_freq.json')
 
-	#Time data visualization
-	dates_Search = []
-	with open(fname, 'r') as f:
-		for line in f:
-			tweet = json.loads(line)
-			terms_only = [term for term in preprocess(tweet.get('text', 'nil'))]
-			if 'search' in terms_only:
-				dates_Search.append(tweet['created_at'])
+    # Time data visualization
+    dates_Search = []
+    with open(fname, 'r') as f:
+        for line in f:
+            tweet = json.loads(line)
+            terms_only = [term for term in preprocess(tweet.get('text', 'nil'))]
+            if 'search' in terms_only:
+                dates_Search.append(tweet['created_at'])
 
-	ones = [1]*len(dates_Search)
+    ones = [1] * len(dates_Search)
 
-	idx = pandas.DatetimeIndex(dates_Search)
-	Search = pandas.Series(ones, idx)
+    idx = pandas.DatetimeIndex(dates_Search)
+    Search = pandas.Series(ones, idx)
 
-	per_minute = Search.resample('1Min', how='sum').fillna(0)
+    per_minute = Search.resample('1Min', how='sum').fillna(0)
 
-	time_chart = vincent.Line(per_minute)
-	time_chart.axis_titles(x="time", y="Freq")
-	time_chart.to_json('time_chart.json')
+    time_chart = vincent.Line(per_minute)
+    time_chart.axis_titles(x="time", y="Freq")
+    time_chart.to_json('time_chart.json')
