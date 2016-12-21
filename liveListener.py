9f1fc9a6f4ae85bca5794dd47400520775eeebbe
Apara <ssapar@gmail.com>
2016-12-21 15:54:07 -0700

    Move Listener to separate file

---
+++ liveListener.py
@@ -0,0 +1,30 @@
+import time
+from tweepy.streaming import StreamListener
+
+runTime = 5 * 60  # 5 minutes
+
+# Listening to Live Tweets
+class Listener(StreamListener):
+    def __init__(self):
+        self.startTime = time.time()
+        self.fname = 'liveStream.json'
+        open(self.fname, 'w').close()
+
+    def on_data(self, data):
+
+        if (time.time() - self.startTime) >= runTime:
+            return False
+
+        try:
+            with open(self.fname, 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print "Error"
+        return True
+
+    def on_error(self, status_code):
+        print status_code
+        if status_code == 420:
+            print "Connection not established"
+        return False
--- mineTweets.py
+++ mineTweets.py
@@ -1,58 +1,63 @@
 import tweepy
-import time
 from tweepy import OAuthHandler
 from tweepy import Stream
-from tweepy.streaming import StreamListener
+from liveListener import Listener
 
-# File containing API and Access credentials
-config_fname = 'config'
+# # File containing API and Access credentials
+# config_fname = 'config'
 
-# App credentials
-with open(config_fname, 'r') as f:
-    consumer_key = f.readline().replace('\n', '')
-    consumer_secret = f.readline().replace('\n', '')
-    access_token = f.readline().replace('\n', '')
-    access_secret = f.readline().replace('\n', '')
+# # App credentials
+# with open(config_fname, 'r') as f:
+#     consumer_key = f.readline().replace('\n', '')
+#     consumer_secret = f.readline().replace('\n', '')
+#     access_token = f.readline().replace('\n', '')
+#     access_secret = f.readline().replace('\n', '')
+#
+# auth = OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_secret)
+#
+# api = tweepy.API(auth)
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+class TweetMiner:
+    def __init__(self, config_fname='config'):
+        _readdetails(config_fname)
+        _authenticate(self)
 
-api = tweepy.API(auth)
+    def choice(self):
+        self.state = None
+        while self.state != '1' or self.state != '2':
+            print "Press 1 to calculate popularity of a phrase. Press 2 to analyze a user profile."
+            self.state = str(raw_input())
+            if self.state == '1' or self.state == '2':
+                break
+            print "Enter a valid choice"
+        #Call functions
 
-runTime = 5 * 60  # 5 minutes
+    def trackLiveTweets(self):
+        print "Enter a key word to track for 5 minutes. Be as specific as possible"
+        self.trackWord = str(raw_input())
+        self.twitter_stream = Stream(self.auth, Listener())
+        self.twitter_stream.filter(track=[self.trackWord])
 
+    def _readdetails(self, config_fname):
+        with open(config_fname, 'r') as f:
+            self.consumer_key = f.readline().replace('\n', '')
+            self.consumer_secret = f.readline().replace('\n', '')
+            self.access_token = f.readline().replace('\n', '')
+            self.access_secret = f.readline().replace('\n', '')
 
-# Listening to Live Tweets
-class MyListener(StreamListener):
-    def __init__(self):
-        self.startTime = time.time()
-
-    def on_data(self, data):
-
-        if (time.time() - self.startTime) >= runTime:
-            return False
-
-        try:
-            with open('liveStream.json', 'a') as f:
-                f.write(data)
-                return True
-        except BaseException as e:
-            print "Error"
-        return True
-
-    def on_error(self, status_code):
-        print status_code
-        if status_code == 420:
-            print "Connection not established"
-        return False
+    def _authenticate(self):
+        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_secret)
+        self.api = tweepy.API(self.auth)
 
 
 if __name__ == "__main__":
-    print "Enter a key word to track for 5 minutes. Be as specific as possible"
-    trackWord = str(raw_input())
+    # print "Enter a key word to track for 5 minutes. Be as specific as possible"
+    # trackWord = str(raw_input())
 
     # Tracking live tweets with keyword
-    twitter_stream = Stream(auth, MyListener())
+    twitter_stream = Stream(auth, Listener())
     twitter_stream.filter(track=[trackWord])
 
     # Gathering tweets of a user
