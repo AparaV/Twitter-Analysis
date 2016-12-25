0f1e7f29e2a14d44ad784d1191cb41d109be3845
Apara <ssapar@gmail.com>
2016-12-25 16:23:20 -0700

    Re-add function to reset global variables

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ app = Flask(__name__)
 
 fname = './tmp/liveStream.json'
 
+
 class Global:
     th = None
     score = -1
     phrase = ""
     finished = False
 
+
 @app.route("/")
 def init():
     return render_template('index.html')
 
+
 @app.route("/", methods=['POST'])
 def calc():
+    resetglobal()
     text = request.form['query']
     run_time = int(request.form['runtime'])
     Global.finished = False
@@ -28,23 +32,35 @@ def calc():
     Global.th.start()
     return render_template('loading.html', text=Global.phrase, time=run_time)
 
+
 @app.route("/result")
 def result():
     return render_template('output.html', text=Global.phrase, pop=Global.score)
+
+
+@app.route("/status")
+def status():
+    return jsonify(dict(status=('finished' if Global.finished else 'running')))
+
 
 def something(text, time):
     Global.score = calculate(text, time)
     Global.finished = True
 
-@app.route("/status")
-def status():
-    return jsonify(dict(status=('finished' if Global.finished else 'running')))
+
+def resetglobal():
+    Global.th = None
+    Global.score = -1
+    Global.phrase = ""
+    Global.finished = False
+
 
 def calculate(text, runTime):
     auth, api = tweetminer.get_credentials()
     tweetminer.get_live_tweets(auth, text, fname=fname, runTime=runTime)
     x = tweetminer.get_popularity(runTime=runTime, fname=fname)
     return x
+
 
 if __name__ == "__main__":
     app.run(debug=True)
