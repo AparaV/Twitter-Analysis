d7316aa44c0eccbe0e8c0024ce3f9ed1bad4d2ee
Apara <ssapar@gmail.com>
2016-12-24 17:42:18 -0700

    Fix config vars

--- tweetminer.py
+++ tweetminer.py
@@ -1,16 +1,16 @@
 import tweepy
 import time
+import os
 from tweepy import OAuthHandler
 from tweepy import Stream
 from liveListener import Listener
 from calculatePopularity import CalculatePopularity
 
-def get_credentials(fname='config'):
-    with open(fname, 'r') as f:
-        consumer_key = f.readline().replace('\n', '')
-        consumer_secret = f.readline().replace('\n', '')
-        access_token = f.readline().replace('\n', '')
-        access_secret = f.readline().replace('\n', '')
+def get_credentials():
+    consumer_key = os.environ.get('CONSUMER_KEY', 'F7QUAALcpxgycKTfo0oqOBE1b')
+    consumer_secret = os.environ.get('CONSUMER_SECRET', 'S2SVeLh2UstahLR93ggeaVbdK6v0ARAeCotT6Q8ng2ZZREOELU')
+    access_token = os.environ.get('ACCESS_TOKEN', '793278461200642048-nIHxACYfkCUZo9AgZxgtiFwedqomvNI')
+    access_secret = os.environ.get('ACCESS_SECRET', 'hrwNcc05PVDRJRbf6qjtxNCwV18R0Wx5RX0OJsJS83XMi')
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
