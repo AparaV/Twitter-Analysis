cfb8a705ae9cfb98cef5b16c7e30d44bcaff0c0c
Apara <ssapar@gmail.com>
2016-12-25 14:54:21 -0700

    Encapsulate global variables inside a class

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 fname = './tmp/liveStream.json'
 
-th = None
-score = -1
-phrase = ""
-finished = False
+class Global:
+    th = None
+    score = -1
+    phrase = ""
+    finished = False
 
 @app.route("/")
 def init():
@@ -19,51 +20,33 @@ def init():
 
 @app.route("/", methods=['POST'])
 def calc():
-    global th
-    global finished
-    global score
-    global phrase
     text = request.form['query']
     run_time = int(request.form['runtime'])
-    finished = False
-    phrase = text
-    th = Thread(target=something, args=(text, run_time))
-    th.start()
-    return render_template('loading.html', text=phrase, time=run_time)
+    Global.finished = False
+    Global.phrase = text
+    Global.th = Thread(target=something, args=(text, run_time))
+    Global.th.start()
+    return render_template('loading.html', text=Global.phrase, time=run_time)
 
 @app.route("/result")
 def result():
-    search = phrase
-    x = score
-    #resetglobal()
+    search = Global.phrase
+    x = Global.score
     return render_template('output.html', text=search, pop=x)
 
 def something(text, time):
-    global finished
-    global score
-    score = calculate(text, time)
-    finished = True
+    Global.score = calculate(text, time)
+    Global.finished = True
 
 @app.route("/status")
 def status():
-    #global finished
-    return jsonify(dict(status=('finished' if finished else 'running')))
+    return jsonify(dict(status=('finished' if Global.finished else 'running')))
 
 def calculate(text, runTime):
     auth, api = tweetminer.get_credentials()
     tweetminer.get_live_tweets(auth, text, fname=fname, runTime=runTime)
     x = tweetminer.get_popularity(runTime=runTime, fname=fname)
     return x
-
-def resetglobal():
-    global th
-    #global score
-    #global phrase
-    global finished
-    th = None
-    #score = -1
-    #phrase = ""
-    finished = False
 
 if __name__ == "__main__":
     app.run(debug=False)
