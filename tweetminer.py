51725bbbc13321e5d625c2b36b491fcc4739c74e
Apara <ssapar@gmail.com>
2016-12-26 15:29:54 -0700

    Remove credentials

--- tweetminer.py
+++ tweetminer.py
@@ -7,10 +7,10 @@ from liveListener import Listener
 from calculatePopularity import CalculatePopularity
 
 def get_credentials():
-    consumer_key = os.environ.get('CONSUMER_KEY', 'F7QUAALcpxgycKTfo0oqOBE1b')
-    consumer_secret = os.environ.get('CONSUMER_SECRET', 'S2SVeLh2UstahLR93ggeaVbdK6v0ARAeCotT6Q8ng2ZZREOELU')
-    access_token = os.environ.get('ACCESS_TOKEN', '793278461200642048-nIHxACYfkCUZo9AgZxgtiFwedqomvNI')
-    access_secret = os.environ.get('ACCESS_SECRET', 'hrwNcc05PVDRJRbf6qjtxNCwV18R0Wx5RX0OJsJS83XMi')
+    consumer_key = os.environ.get('CONSUMER_KEY')
+    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    access_token = os.environ.get('ACCESS_TOKEN')
+    access_secret = os.environ.get('ACCESS_SECRET')
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
